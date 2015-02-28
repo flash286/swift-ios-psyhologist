fc32f5b6d7fe8ee1f2662e3a64684c1638800558
flash286 <flash286@gmail.com>
2015-02-28 23:43:22 +0700

    Initial Commit

---
+++ PsyhologistTests/PsyhologistTests.swift
@@ -0,0 +1,36 @@
+//
+//  PsyhologistTests.swift
+//  PsyhologistTests
+//
+//  Created by Брызгалов Николай on 28.02.15.
+//  Copyright (c) 2015 Bryzgalov Nikolay. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PsyhologistTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
